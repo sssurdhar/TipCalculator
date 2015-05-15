9c04e4ebcdc5af451706195cc358195199b793fa
sssurdhar <sunny.surdhar80@gmail.com>
2015-05-15 10:05:58 -0700

    Created File

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Sunny Surdhar on 2015-05-15.
+//  Copyright (c) 2015 Sunny Surdhar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TipCalculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TipCalculator
+//
+//  Created by Sunny Surdhar on 2015-05-15.
+//  Copyright (c) 2015 Sunny Surdhar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
